2bba70e71647b3449edbd982daa751b062aee741
HemersonRafael <rafaelpontes1995@gmail.com>
2019-04-08 19:26:07 -0300

    add projectController

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,7 +1,9 @@
 const express = require('express')
 //const User = require('../models/user')
 const router = express.Router();
+const authMiddeleware = require('../middlewares/auth')
 
+router.use(authMiddeleware);
 router.get('/', (req, res) =>{
     res.send({ok: true});
 });
